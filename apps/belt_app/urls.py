82bf2917a91977d6733bf01c75c143b3b6af702f
harshi agarwal <harshita.agarwal211@gmail.com>
2016-10-20 13:19:24 -0700

    third commit

--- apps/belt_app/urls.py
+++ apps/belt_app/urls.py
@@ -4,4 +4,5 @@ from .import views
 urlpatterns = [
     # url(r'^admin/', admin.site.urls),
     url(r'^$',views.index,name="index"),
+    url(r'^register,views.register,name="register"),
 ]
--- apps/belt_app/views.py
+++ apps/belt_app/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request,"belt_app/index.html")
+def register(request):
+    if request.method =="POST":
+        User.objects.register(request.POST)
+        
